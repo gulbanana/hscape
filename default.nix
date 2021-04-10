3f3cd0d50f05ef56c0f742b42786eaf474ea0e92
banana <castiglione@mac.com>
2021-04-10 17:58:03 +0800

    minify, tree-shake and compress the GHCJS output

--- default.nix
+++ default.nix
@@ -3,12 +3,18 @@ with import ./miso.nix;
 stdenv.mkDerivation {
   name = "scape";
   src = ./.;
-  buildInputs = [release];
+  buildInputs = [release pkgs.closurecompiler pkgs.zopfli];
   buildPhase = ''  
     mkdir $out
     cp ${release}/bin/app.jsexe/* $out/
   '';
   installPhase = ''
+    rm $out/index.html
     cp wwwroot/* $out/
+    ${pkgs.closurecompiler}/bin/closure-compiler --compilation_level ADVANCED_OPTIMIZATIONS \
+      --jscomp_off=checkVars \
+      --externs=$out/all.js.externs \
+      $out/all.js > $out/all.min.js
+    ${pkgs.zopfli}/bin/zopfli $out/all.min.js
   '';
 }
